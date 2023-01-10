9ea97ed1a6e9bf85a12df4fffc1f9f7d760ea3ec
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-01-10 19:58:59 +0300

    Completed the Adding Behavior With Methods Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Your code here
+
+class Cat {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says meow!`
+    }
+  }
+  
+  class Dog {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        return `${this.name} says woof!`
+    }
+  }
+  
+  class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() {
+        if (this.sex === "male") {
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else {
+            return `${this.name} says squawk!`
+        }        
+    }
+  }
+
+  let rose = new Cat('rose', 'female')
+  let koukou = new Bird('koukou', 'male')
+  let cocoriko = new Bird('cocoriko', 'female')
+  console.log(rose.speak())
+  console.log(koukou.speak())
+  console.log(cocoriko.speak())
+
